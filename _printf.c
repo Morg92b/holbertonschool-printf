75f4f7949874eb75e89ced130748b8d8cefa65c3
Wqkz <guillaume.farina@hotmail.com>
2024-07-22 17:09:52 +0200

    created the function printf for 'c','s' and '%'

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf - Create 'function printf with struct and variadic functions'
+ * @format: list of types of arguments passed to the function
+ */
+
+int _printf(const char *format, ...)
+{
+	format_specifier_t specifiers[] = {
+		{'c', _printf_char},
+		{'s', _printf_string},
+		{'%', _printf_percentage},
+		{0, NULL},
+	};
+
+	va_list args;
+	int index = 0, j;
+
+	va_start(args, format);
+	while (format && format[index])
+	{
+		if ( format[index] == '%')
+		{
+			index++;
+			j = 0;
+		while (specifiers[j].specifier)
+		{
+			if (format[index] == specifiers[j].specifier)
+			{
+				specifiers[j].print_func(args);
+				break;
+			}
+			j++;
+		}
+		}
+		else
+		{
+			_putchar(format[index]);
+		}
+		index++;
+	}
+	va_end(args);
+	return (0);
+}
