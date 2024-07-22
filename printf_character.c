016c0697d189203f5cac02ce862d00d23c747caf
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-22 14:21:22 +0200

    add files.c for function to print a character

---
+++ printf_character.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *printf_char - function that print one character*
+ *@args: the argument list containing the character to print
+ *Return: the number of characters printed (always 1)
+ */
+
+int _printf_char(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
