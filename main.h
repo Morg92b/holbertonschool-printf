c35d3b922c7ce4127775d07173fd42b592eec160
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-22 16:23:55 +0200

    add struct for specifiers to functions

--- main.h
+++ main.h
@@ -6,6 +6,19 @@
 #include <string.h>
 #include <stdarg.h>
 
+/**
+ * struct format_specifier - struct to map specifiers to functions.
+ * @specifier: format specifier character.
+ * @print_func: pointer to the function that handles printing for specifier.
+ * Despcription: associate format specifiers with corresponding print function.
+ */
+
+typedef struct format_specifier
+{
+	char specifier;
+	int (*print_func)(va_list args);
+} format_specifier_t;
+
 int _printf(const char *format, ...);
 
 int _printf_char(va_list args);
