1e754ad78b1bf2a672ae0719d1ddf6109703bf1b
Wqkz <guillaume.farina@hotmail.com>
2024-07-23 11:05:18 +0200

    fix betty

--- printf_string.c
+++ printf_string.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _printf_string - prints a string of char
+ * _printf_string - Prints 'string of char'
  * @args: character of the arguments
- * @Return: return total numbers of char print
+ * Return: _print_string of count
  */
 
 int _printf_string(va_list args)
 {
 	int count = 0; /* Initializes a counter */
-	char *str = va_arg(args, char*); /* Retrieves the next argument from the args list */
+	char *str = va_arg(args, char*);
+	/* Retrieves the next argument from the args list */
 
 	if (str == NULL)
 		str = ("NULL"); /* to avoid undefined behavior, if NULL */
