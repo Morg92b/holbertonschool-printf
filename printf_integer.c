893bb843c0c40aa899d3a8f862e7b023e82f6a25
Wqkz <guillaume.farina@hotmail.com>
2024-07-24 12:30:56 +0200

    try to fix the count of buffer

--- printf_integer.c
+++ printf_integer.c
@@ -13,7 +13,7 @@ int _printf_integer(va_list args)
 
 	int count = 0; /* count: it is counter the number print */
 	int temp; /* it is a variable temp for stock the num */
-	char buffer[50]; /* it is array containing the digits of the integer */
+	char buffer[128]; /* it is array containing the digits of the integer */
 	int index = 0;
 
 	if (num < 0) /* For manage the negative number */
