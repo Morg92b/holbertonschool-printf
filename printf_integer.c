d990b9e805b1f8cdc3e65020c95c2d3c06578b40
Wqkz <guillaume.farina@hotmail.com>
2024-07-24 13:04:57 +0200

    try to fix code

--- printf_integer.c
+++ printf_integer.c
@@ -7,12 +7,12 @@
  */
 int _printf_integer(va_list args)
 {
-	/*long*/ int num = va_arg(args, /*long*/int);
+	long int num = va_arg(args, long int);
 
 	/* this is the entire extract from the variadic argument list */
 
 	int count = 0; /* count: it is counter the number print */
-	/*long int temp;  it is a variable temp for stock the num */
+	long int temp; /*it is a variable temp for stock the num */
 	char buffer[50]; /* it is array containing the digits of the integer */
 	int index = 0;
 
@@ -28,11 +28,11 @@ int _printf_integer(va_list args)
 		_putchar('0');
 		return (1);
 	}
-	/*temp = num; Extract the digits and the buffer in reverse order */
+	temp = num; /*Extract the digits and the buffer in reverse order */
 	while (num > 0)
 	{
-		buffer[index++] = (num % 10) + '0';
-		num /= 10;
+		buffer[index++] = (temp % 10) + '0';
+		temp /= 10;
 	}
 
 	for (--index; index >= 0; index--) /* For print the number in good order */
